20c9eb774014d78d4ddaf5d8b5efae1612f08674
royturpo123 <>
2021-04-26 19:59:01 +0000

    mi tarea

---
+++ AreaTriangulo.rb
@@ -0,0 +1,11 @@
+# Definir Variables y Otros
+puts "Hola Mundo"
+#Datos de Entrada por dispositivos de entrada
+print "Ingrese Base:"
+b=gets.to_i
+print "Ingrese Altura:"
+h=gets.to_i
+#Proceso
+area=(b*h)/2
+#Datos de Salida
+puts "El area del triangulo es: #{area} m2"
---
+++ arePoraltura.py
@@ -0,0 +1,11 @@
+import java.util.scanner;
+class areaTriangulo{
+  #definicion de variables u otros
+print("Ejercicio 01: Area Triangulo")
+#Datos de Entrada
+b=int(input("Ingrese Base:"))
+h=int(input("Ingrese Altura:"))
+#Proceso
+area=(b*h)/2
+#Datos de Salida
+print("El area es: ", area)
